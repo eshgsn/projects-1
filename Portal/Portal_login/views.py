2628df97fa95d18369eb280e72423cd09713f5b2
Aabhishek29 <starkabhishek29@gmail.com>
2021-10-10 16:25:42 +0530

    third commit

--- Portal/Portal_login/views.py
+++ Portal/Portal_login/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request,'Home.html')
+
+def getUserLogin(request):
+    # if request.methord == 'post':
+    #     pass
+    username = request.POST['username']
+    password = request.POST['password']
+    print("Hello This is")
+    return HttpResponse("<h1>Working</h1>"+username+" "+password)
